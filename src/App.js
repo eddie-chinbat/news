b11b0fcd86c0bc022d7e758bbec42df462ba40cb
Edy <erdenebyr@gmail.com>
2020-06-06 13:18:00 -0500

    Footer component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './Components/Header/Header';
 import Body from './Components/Body/Body';
+import Footer from './Components/Footer/Footer';
 
 function App(){
-  return (
-    <Header/>,
-    <Body val="Edy"/>
+  return ( 
+    <div>
+      <Header/>
+      <Body val="Edy"/>
+      <Footer/>
+    </div>
   );
 }
 
--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 function Body(props){
     return(
-    <div>this is body {props.val}</div>
+    <div>BODY PART</div>
     );
+}
+
+export const Test1 = () => {
+    return(
+        <h1>hello this is test</h1>
+    )
 }
 
 export default Body;
